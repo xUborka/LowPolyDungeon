e24983ed4ea1bd8428575562ff42f96386af089a
xUborka <xgurke@gmail.com>
2022-10-31 11:54:59 +0100

    feat: :sparkles: NavMesh movement, animation update

--- Assets/EnemyMovementScript.cs
+++ Assets/EnemyMovementScript.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovementScript : MonoBehaviour
 {
     public Animator anim;
+
+    private NavMeshAgent navMeshAgent;
+    [SerializeField] private Transform movePositionTransform;
+    [SerializeField] private float attackTimer = 0.0f;
+    [SerializeField] private bool attacking = false;
+
+
+    private void Awake(){
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +27,29 @@ public class EnemyMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (attacking){
+            attackTimer += Time.deltaTime;
+            if (attackTimer > 2f && Vector3.Distance(movePositionTransform.position, gameObject.transform.position) >= navMeshAgent.stoppingDistance){
+                attackTimer = 0f;
+                attacking = false;
+            }
+        }
+
+        if (!attacking){
+            navMeshAgent.destination = movePositionTransform.position;
+        }
+
+        anim.SetFloat("Speed", navMeshAgent.velocity.magnitude);
+        anim.SetBool("Attack", attacking);
+
         
+
+        // Below stopping distance = Melee range
+        if (Vector3.Distance(movePositionTransform.position, gameObject.transform.position) < navMeshAgent.stoppingDistance){
+            Vector3 direction = (movePositionTransform.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10.0f);
+            attacking = true;
+        }
     }
 }
